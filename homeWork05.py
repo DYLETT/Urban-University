c89c86466593c66adbcbdadd68348f7bfb205432
DYLETT <112850430+DYLETT@users.noreply.github.com>
2024-04-11 11:51:41 +0700

    Add files via upload

---
+++ homeWork05.py
@@ -0,0 +1,14 @@
+print("Работа со списками: ")
+my_list = ['Nike', 'NB', 'Adidas', 'Reebok', "Puma", "Balenciaga"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'Jordan'
+print(my_list)
+print('Работа со словарями: ')
+my_dict = {'Parents': 'Родители', 'Pants': 'Штаны', 'Paints': 'Краски'}
+print(my_dict)
+print(my_dict['Parents'])
+my_dict['Pairs'] = 'Пары'
+print(my_dict)
